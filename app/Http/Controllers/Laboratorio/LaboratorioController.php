86df3c05cc93e738388db055c1028755dad5f27b
extah <extah23@gmail.com>
2021-09-02 22:55:32 -0300

    a

--- app/Http/Controllers/Laboratorio/LaboratorioController.php
+++ app/Http/Controllers/Laboratorio/LaboratorioController.php
@@ -1,15 +1,16 @@
 <?php
 
-namespace App\Http\Controllers;
-
+namespace App\Http\Controllers\Laboratorio;
 use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
 
 class LaboratorioController extends Controller
 {
     //
     public function index(){
 
-    	return view('laboratorio.laboratorio', compact());
+    	return view('laboratorio.laboratorio');
     
     }
 }
