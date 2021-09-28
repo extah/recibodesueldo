8995d6bc6c9a20c5d7889c0a396ba3fa7835a485
extah <extah23@gmail.com>
2021-09-28 16:49:17 -0300

    eliminando contenido

--- resources/views/inicio/inicio.blade.php
+++ resources/views/inicio/inicio.blade.php
@@ -599,27 +599,6 @@ a.btn-goback:hover {
         $(this).removeClass("idle").addClass("active");
         });
     });
-    
-    // $(function() {
-    //     $(".btn-signup").click(function() {
-    //   $(".nav_inicio").toggleClass("nav-up_inicio");
-    //   $(".form-signup-left").toggleClass("form-signup-down");
-    //   $(".success").toggleClass("success-left"); 
-    //   $(".frame").toggleClass("frame-short");
-    //     });
-    // });
-    
-    // $(function() {
-    //     $(".btn-signin").click(function() {
-    //   $(".btn-animate").toggleClass("btn-animate-grow");
-    //   $(".welcome").toggleClass("welcome-left");
-    //   $(".cover-photo").toggleClass("cover-photo-down"); 
-    //   $(".frame").toggleClass("frame-short");
-    //   $(".profile-photo").toggleClass("profile-photo-down");
-    //   $(".btn-goback").toggleClass("btn-goback-up");
-    //   $(".forgot").toggleClass("forgot-fade");
-    //     });
-    // });
 
 </script>
 <script>
